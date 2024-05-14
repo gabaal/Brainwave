9f0d991057ab749e3d2306820df438ed7fffbffd
gerald <geraldbaalham@live.co.uk>
2024-05-14 11:07:11 +0100

    lg navigation complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import ButtonGradiant from "./assets/svg/ButtonGradient";
 import Button from "./components/Button";
+import Header from "./components/Header";
+
 const App = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <div className="pt-[4.75rem] lg:pt-[5.35rem] overflow-hidden">
-        <Button className="mt-10" href="#login">
-          Something
-        </Button>
+        <Header />
       </div>
       <ButtonGradiant />
     </>
