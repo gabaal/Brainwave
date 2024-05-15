ad6edd21b03bf010271cfe7a7b0da1459055d1b6
gerald <geraldbaalham@live.co.uk>
2024-05-15 11:58:25 +0100

    Hero section complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import ButtonGradiant from "./assets/svg/ButtonGradient";
-import Button from "./components/Button";
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
     <>
       <div className="pt-[4.75rem] lg:pt-[5.35rem] overflow-hidden">
         <Header />
+        <Hero />
       </div>
       <ButtonGradiant />
     </>
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -11,14 +11,17 @@ const Section = ({
   return (
     <div
       id={id}
-      className={`relative ${customPaddings} || 'py-10 lg:py-16 xl:py-20 ${
-        crosses ? "lg:py-32 xl:py-40" : ""
-      } ${className || ""}`}
+      className={`
+      relative 
+      ${
+        customPaddings ||
+        `py-10 lg:py-16 xl:py-20 ${crosses ? "lg:py-32 xl:py-40" : ""}`
+      } 
+      ${className || ""}`}
     >
       {children}
 
       <div className="hidden absolute top-0 left-5 w-0.25 h-full bg-stroke-1 pointer-events-none md:block lg:left-7.5 xl:left-10" />
-
       <div className="hidden absolute top-0 right-5 w-0.25 h-full bg-stroke-1 pointer-events-none md:block lg:right-7.5 xl:right-10" />
 
       {crosses && (
@@ -26,7 +29,7 @@ const Section = ({
           <div
             className={`hidden absolute top-0 left-7.5 right-7.5 h-0.25 bg-stroke-1 ${
               crossesOffset && crossesOffset
-            } pointer-events-none lg:block xl:l-10 right-10`}
+            } pointer-events-none lg:block xl:left-10 right-10`}
           />
           <SectionSvg crossesOffset={crossesOffset} />
         </>
