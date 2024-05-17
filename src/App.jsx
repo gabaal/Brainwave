84e6f1225e55c7a4bc391ed9168ed30898ca683a
gerald <geraldbaalham@live.co.uk>
2024-05-17 11:45:13 +0100

    Collaboration section complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import ButtonGradiant from "./assets/svg/ButtonGradient";
+import Benefits from "./components/Benefits";
+import Collaboration from "./components/Collaboration";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
-import Benefits from "./components/Benefits";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
         <Header />
         <Hero />
         <Benefits />
+        <Collaboration />
       </div>
       <ButtonGradiant />
     </>
